7f4357c14ff0febb310bd454730b6d6894aa559a
KwangJa-dev <eewt123450@naver.com>
2022-04-27 18:00:06 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-
+import Tether from '../truffle_abis/Tether.json'
 function App(){
 
     const [account, setAccount] = useState("");
+    const [tether, setTether] = useState();
 
     const UNSAFE_componentWillMount = async function(){
         await loadWeb3();
@@ -23,9 +24,21 @@ function App(){
     const loadBlockChainData =  async function(){
         const web3 = window.web3;
         const account = await web3.eth.getAccounts();
-        setAccount(account[0]);
-        const networkId = web3.eth.net.getId();
-        console.log(networkId)
+        setAccount(account)
+        const networkId = await web3.eth.net.getId();
+        const tetherData = Tether.networks[networkId];
+        if(tetherData){
+            const tether = new web3.eth.Contract(Tether.abi, tetherData.address);
+            setTether(tether);
+            console.log("111")
+            console.log(tether)
+            let ththerBalance = await tether.methods.balanceOf(account[0]).call();
+            console.log("111")
+            console.log(ththerBalance)
+            console.log(ththerBalance.toString())
+            console.log(ththerBalance.toString())
+        }
+        
     }
 
     useEffect(()=>{
